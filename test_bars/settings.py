6946d6c0f9cfbbce5c298d7821339e721c6c66f3
fon_fab <fonfabular@gmail.com>
2019-10-18 18:57:06 +0300

    no message

--- test_bars/settings.py
+++ test_bars/settings.py
@@ -118,8 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+os.makedirs(STATIC_TMP, exist_ok=True)
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
